10fbbba5ebdcfa0187f5668afdde134ee948fe78
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-04-26 01:39:29 +0200

    react additions

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,22 +1,20 @@
-import React, { component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { render } from 'react-dom';
+import React, { Component } from "react";
+import { render } from "react-dom";
 
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-
   }
+
   render() {
     return (
       <div>
-        <h1>Test App</h1>
+        <h1>hello world</h1>
       </div>
     );
   }
 }
 
-
-const appDiv = getElementByID('app');
+const appDiv = document.getElementById("app");
 render(<App />, appDiv);
